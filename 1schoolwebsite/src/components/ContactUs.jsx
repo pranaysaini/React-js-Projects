ab3581ee7c1c1cae1d6a2feb2220f47e49ebce81
Pranay Saini <sainipranay36@gmail.com>
2024-08-10 11:23:03 +0530

    React-js

--- 1schoolwebsite/src/components/ContactUs.jsx
+++ 1schoolwebsite/src/components/ContactUs.jsx
@@ -1,7 +1,28 @@
+import { useState } from 'react';
+import Swal from 'SweetAlert2';
 import Navbar from './Navbar'
 import Footer from './Footer'
 
 const ContactUs = () => {
+
+    const [name, setName] = useState("");
+    const [num, setNum] = useState("");
+    const [mail, setMail] = useState("");
+
+    const newValue = (event) => {
+        event.preventDefault();
+        console.log(name, num, mail)
+
+        Swal.fire({
+            icon: "success",
+            title: "Thanks",
+            text: "Thank you! We Will Reach You Soon"
+
+        })
+        
+
+        
+    }
     return (
         <div>
             <Navbar />
@@ -9,24 +30,27 @@ const ContactUs = () => {
             <div className="flex p-6">
                 <img src="contact.png" className="h-full w-1/2" />
                 
-                <form className=" w-full flex flex-col gap-4 mt-4">
+                <form className=" w-full flex flex-col gap-4 mt-4" onSubmit={newValue}>
 
                     <div className="flex flex-col ml-36 gap-2">
                         <label className='text-xl font-serif font-bold ml-1'>Full Name</label>
                         <input type="text" className="border border-double border-black rounded-xl h-11 w-72 p-2 " 
-                                           placeholder="Enter Name Here" />
+                                           placeholder="Enter Name Here" 
+                                           onChange={(event) => setName(event.target.value)} />
                     </div>
 
                     <div className="flex flex-col ml-36 gap-2">
                         <label className='text-xl font-bold font-serif ml-1'>Phone Number</label>
                         <input type="text" className="border border-double border-black rounded-xl h-11 w-72 p-2" 
-                                           placeholder="Enter Phone Number" />
+                                           placeholder="Enter Phone Number" 
+                                           onChange={(event) => setNum(event.target.value)} />
                     </div>
 
                     <div className="flex flex-col ml-36 gap-2">
                         <label className='text-xl font-bold font-serif ml-1'>E-Mail</label>
                         <input type="text" className="border border-double border-black rounded-xl h-11 w-72 p-2" 
-                                           placeholder="abcd@mail.com" />
+                                           placeholder="abcd@mail.com" 
+                                           onChange={(event) => setMail(event.target.value)} />
                     </div>
 
                     <button className='bg-indigo-600 h-11 w-28 rounded-xl text-white text-lg font-bold font-mono ml-36 mt-3 hover:bg-indigo-500'>SUBMIT</button>
--- 1schoolwebsite/src/components/Holidays.jsx
+++ 1schoolwebsite/src/components/Holidays.jsx
@@ -1,14 +1,103 @@
-import Navbar from './Navbar'
-import Footer from './Footer'
+import Navbar from './Navbar';
+import Footer from './Footer';
 
 const Holidays = () => {
+
+    const holidays = [
+        {
+            title: 'Summer Vacation 2024',
+            duration: '2 Months',
+            date: '10/04/2024'
+        },
+        {
+            title: 'Winter Break 2024',
+            duration: '1 Month',
+            date: '12/12/2024'
+        },
+        
+        {
+            title: 'Summer Vacation 2024',
+            duration: '2 Months',
+            date: '10/04/2024'
+        },
+        {
+            title: 'Winter Break 2024',
+            duration: '1 Month',
+            date: '12/12/2024'
+        },
+        {
+            title: 'Summer Vacation 2024',
+            duration: '2 Months',
+            date: '10/04/2024'
+        },
+        {
+            title: 'Winter Break 2024',
+            duration: '1 Month',
+            date: '12/12/2024'
+        },
+
+        {
+            title: 'Winter Break 2024',
+            duration: '1 Month',
+            date: '12/12/2024'
+        },
+
+        {
+            title: 'Winter Break 2024',
+            duration: '1 Month',
+            date: '12/12/2024'
+        },
+
+        {
+            title: 'Winter Break 2024',
+            duration: '1 Month',
+            date: '12/12/2024'
+        },
+
+        {
+            title: 'Winter Break 2024',
+            duration: '1 Month',
+            date: '12/12/2024'
+        },
+
+        
+        
+
+    ];
+
     return (
         <div>
             <Navbar />
-            Meri Holidays
-            <Footer />
+            
+            <div className="mt-16 h-20 w-60 mx-auto">
+                <h1 className="text-4xl font-bold">Your Holidays</h1>
+            </div>
 
+            <table className="border border-collapse border-black mx-auto mt-5 mb-32   w-2/3 text-center ">
+                <thead className="bg-slate-400">
+                    <tr className="text-2xl">
+                        <th className='border border-collapse p-4'>S.No.</th>
+                        <th className='border border-collapse'>Title</th>
+                        <th className='border border-collapse'>Duration</th>
+                        <th className='border border-collapse'>Date</th>
+                    </tr>
+                </thead>
+
+                <tbody>
+                    {holidays.map((items, index) => (
+                        <tr key={index}>
+                            <td className='border border-collapse p-6'>{index + 1}</td>
+                            <td className='border border-collapse'>{items.title}</td>
+                            <td className='border border-collapse'>{items.duration}</td>
+                            <td className='border border-collapse'>{items.date}</td>
+                        </tr>
+                    ))}
+                </tbody>
+            </table>
+
+            <Footer />
         </div>
     );
-}
-export default Holidays
+};
+
+export default Holidays;
