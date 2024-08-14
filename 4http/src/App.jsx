3cdd57ecf9cd9ede0544af5bdb1789a004531a0e
Pranay Saini <sainipranay36@gmail.com>
2024-08-14 18:21:59 +0530

    react-js

--- 4http/src/App.jsx
+++ 4http/src/App.jsx
@@ -5,10 +5,25 @@ import 'font-awesome/css/font-awesome.min.css'
 function App() {
   const [newData, setNewData] = useState([])
   const[load, setLoad] = useState(false)
+  const [fetchData, setFetchData] = useState(false)
+  const [count, setCount] = useState(0)
+  
 
-  // useEffect(() => {
-  //     getData()
-  // }, [])
+  useEffect(() => {
+      getData()
+  }, [fetchData])
+
+  useEffect(() => {
+    const timer = setInterval(() => {
+      setCount(prevCount => prevCount+1)
+    }, 500)
+    
+    // Clear Function
+    return () => {
+      clearInterval(timer)
+    }
+
+  }, [count])
 
   const getData = () => {
 
@@ -28,18 +43,33 @@ function App() {
       setLoad(false)
     })
   }
+  // flex flex-wrap justify-center items-center gap-5 w-full
 
   return (
     <>
-      <div className='h-screen w-full'>
-        <h1 className='text-3xl text-center font-bold mt-20'>HTTP Request</h1>
-
-        <button className="bg-indigo-700 rounded-md text-white text-lg font-bold h-12 w-36 ml-96 mt-10 hover:bg-indigo-500"
-                onClick={getData}>
-                  
-                  GET DATA
+      <div className=' w-full bg-green-400 pt-24 pb-24'>
+        <h1 className='text-3xl text-center font-bold'>HTTP Request</h1>
         
-        </button>
+        <div className='flex justify-center items-center'>
+          <button className="bg-indigo-700 rounded-md text-white text-lg font-bold h-12 w-36 mr-10 mt-10 hover:bg-indigo-500"
+                    onClick={getData}>
+                      
+                      GET DATA
+            
+            </button>
+
+            <p className='text-2xl text-center font-bold mt-10'>Total Count is:  {count}</p>
+
+            <button className="bg-indigo-700 rounded-md text-white text-lg font-bold h-12 w-36 ml-10 mt-10 hover:bg-indigo-500"
+                    onClick={() => setFetchData(!fetchData)}>
+                      
+                      NEW DATA
+            
+            </button>
+
+        </div>
+          
+        
                 {
                   load && (
                     <div className='flex justify-center items-center mt-10'>
@@ -48,14 +78,20 @@ function App() {
                   )
                 }
 
-                <div className="mt-20 w-auto mx-auto shadow-[0px_4px_10px_rgba(0,0,0,0.3)] p-7">
+                <div className='grid grid-cols-1 sm:grid-cols-2 md:grid-cols-2 mt-3 gap-x-36 w-1/2 mx-auto'>
+                  
 
                   {
                     newData.map((items, index) => (
                       <div key={index}>
+
+                        <div className="mt-20 w-96 mx-auto shadow-[0px_4px_10px_rgba(0,0,0,0.3)] p-7 bg-rose-400">
                           <h2 className='text-2xl font-bold'>{items.name}</h2>
-                          <p>User Name: {items.username}</p>
-                          <p>E-mail: {items.email}</p>
+                          <p>User Name : {items.username}</p>
+                          <p>E-mail : <span className='text-blue-700 underline'>{items.email}</span></p>
+
+                        </div>
+                          
                       </div>
 
                     ))
